edc9a6ad25d0a1733bcace623e787271b967478e
sami-sheikh <70857948+sami-sheikh@users.noreply.github.com>
2021-12-28 09:02:28 +0530

    Add files via upload

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+function PageNotFound() {
+  const style = {
+    marginTop: "5rem",
+    fontSize: "5rem",
+    textAlign: "center",
+  };
+
+  return <h2 style={style}>Page Not Found</h2>;
+}
+
+export default PageNotFound;
