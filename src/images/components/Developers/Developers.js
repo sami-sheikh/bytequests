e73eee783bd3057ba73b1cace8aa8c826556570a
sami-sheikh <70857948+sami-sheikh@users.noreply.github.com>
2021-12-28 08:59:44 +0530

    Add files via upload

---
+++ src/images/components/Developers/Developers.js
@@ -0,0 +1,18 @@
+function Developers() {
+  return (
+    <div>
+      <div className="card-info">
+        <p>Lorem, ipsum dolor</p>
+        <h2>Developers On Demand</h2>
+      </div>
+
+      <section className="container">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+      </section>
+    </div>
+  );
+}
+
+export default Developers;
