870ef485b2a2486132663383dc1cca570603966d
sami-sheikh <70857948+sami-sheikh@users.noreply.github.com>
2021-12-28 09:02:52 +0530

    Add files via upload

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,18 @@
+function Portfolio() {
+  return (
+    <div>
+      <div className="card-info">
+        <p>Lorem, ipsum dolor</p>
+        <h2>Portfolio / Case Studies</h2>
+      </div>
+
+      <section className="container">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+      </section>
+    </div>
+  );
+}
+
+export default Portfolio;
