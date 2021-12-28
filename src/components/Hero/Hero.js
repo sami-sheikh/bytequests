e715d9740029757ff9312b304a20870054dc8290
sami-sheikh <70857948+sami-sheikh@users.noreply.github.com>
2021-12-28 09:01:49 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,8 @@
+import hero2 from "../../images/hero2.png";
+import classes from "./Hero.module.css";
+
+function Hero() {
+  return <div className={classes.hero}>Hero Section</div>;
+}
+
+export default Hero;
