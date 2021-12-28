3cfad7487a7904c417b29eb0c0d7b05efd3e93d3
sami-sheikh <70857948+sami-sheikh@users.noreply.github.com>
2021-12-28 09:03:12 +0530

    Add files via upload

---
+++ src/components/Products/Products.js
@@ -0,0 +1,20 @@
+import hero2 from "../../images/hero2.png";
+
+function Products() {
+  return (
+    <div>
+      <div className="card-info">
+        <p>Lorem, ipsum dolor</p>
+        <h2>Products</h2>
+      </div>
+
+      <section className="container">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+      </section>
+    </div>
+  );
+}
+
+export default Products;
